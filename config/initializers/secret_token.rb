03e4a6773d807d5da85bd79d5bbe2eb7e9c4a03f
jardix22 <jardix22@gmail.com>
2012-03-06 11:09:41 +0100

    setting gems.. to work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveadminCancanRoles::Application.config.secret_token = '9ba5e9ec563229ee621acaa829783d8c0d591a57561cc86a749a02aae40edc1bc3d8d4b74f07daf7ed727c6dd79bb41245e4ee2f29616c2aa1dfa2ae75926750'
